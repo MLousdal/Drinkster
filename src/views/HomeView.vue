19f2fcbdcca045a1eea4984d9b5a7df19172ec3e
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2023-11-05 19:59:23 +0100

    Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="text-3xl font-bold underline">This is the home page</header>
+  <main>
+  </main>
+</template>
---
+++ src/views/OverUnderView.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>This is the "{{ $route.name }}" page</h1>
+</template>
+<script setup lang="ts">
+
+</script>
