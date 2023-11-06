74520fa5beeb67e6be924f0f58f5f9558a35e7c6
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2023-11-06 13:45:33 +0100

    tilt

--- src/components/PlayingCard.vue
+++ src/components/PlayingCard.vue
@@ -1,9 +1,28 @@
 <template>
-  <img :src="`/playing_cards/fronts/${card}.svg`" :alt="card">
+  <img :src="`/playing_cards/fronts/${card}.svg`" :alt="card" ref="cardElm" />
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import VanillaTilt from 'vanilla-tilt'
+
 defineProps<{
   card: string
 }>()
+
+const cardElm = ref(null)
+
+onMounted(() => {
+  if (!cardElm.value) return
+  VanillaTilt.init(cardElm.value, {
+    max: 15,
+    perspective: 2000,
+    speed: 300,
+    gyroscope: true,
+    gyroscopeMinAngleX: -15,
+    gyroscopeMaxAngleX: 15,
+    gyroscopeMinAngleY: -15,
+    gyroscopeMaxAngleY: 15
+  })
+})
 </script>
